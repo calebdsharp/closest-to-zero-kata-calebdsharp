dd78cc0a771c522c0b414bd9842c82c8e46107fc
Caleb <sharpcaleb007@gmail.com>
2020-01-29 20:42:20 -0500

    submit

--- src/main/java/katas/ClosestToZeroFinder.java
+++ src/main/java/katas/ClosestToZeroFinder.java
@@ -4,8 +4,19 @@ import java.util.List;
 
 public class ClosestToZeroFinder {
 
-  public int findClosestToZero(List<Integer> integers) {
-    return 1;
-  }
-
+	public int findClosestToZero(List<Integer> integers) {
+		int closestIndex = 0;
+		int diff = Integer.MAX_VALUE;
+		for (int i = 0; i < integers.size(); i++) {
+			int abs = Math.abs(integers.get(i));
+			if (abs < diff) {
+				closestIndex = i;
+				diff = abs;
+			} else if (abs == diff && integers.get(i) > 0 && integers.get(closestIndex) < 0) {
+				// same distance to zero but positive
+				closestIndex = i;
+			}
+		}
+		return (integers.get(closestIndex));
+	}
 }
--- src/test/java/katas/ClosestToZeroFinderTest.java
+++ src/test/java/katas/ClosestToZeroFinderTest.java
@@ -21,7 +21,43 @@ public class ClosestToZeroFinderTest {
     //Assert
     assertEquals(1, result);
 
-    //Try this out!  Look up AssertJ for a new way to write assertions!  If this scares you stick to the assert as above.
+    //Try this out!  Look up AssertJ for a new way to write assertions! If this scares you stick to the assert as above.
     assertThat(result).isEqualTo(1);
+  }
+  @Test
+  public void givenMoreThanTwoPositiveNumbers_ReturnLowest() {
+	  ClosestToZeroFinder undertest = new ClosestToZeroFinder();
+	  List<Integer> testData = Arrays.asList(4, 6, 5, 2, 9, 35);
+	  
+	  int result = undertest.findClosestToZero(testData);
+	  
+	  assertThat(result).isEqualTo(2);
+  }
+  @Test
+  public void givenPositiveAndNegativeNumbers_ReturnClosestToZero() {
+	  ClosestToZeroFinder undertest = new ClosestToZeroFinder();
+	  List<Integer> testData = Arrays.asList(-4, 6, 5, -9, 35);
+
+	  int result = undertest.findClosestToZero(testData);
+	  
+	  assertThat(result).isEqualTo(-4);
+  }
+  @Test
+  public void givenPositiveAndNegativeNumberAreSameDistance_ReturnPositive() {
+	  ClosestToZeroFinder undertest = new ClosestToZeroFinder();
+	  List<Integer> testData = Arrays.asList(-4, 6, 5, -9, 35, 4);
+
+	  int result = undertest.findClosestToZero(testData);
+	  
+	  assertThat(result).isEqualTo(4);
+  }
+  @Test
+  public void givenListContainsZero() {
+	  ClosestToZeroFinder undertest = new ClosestToZeroFinder();
+	  List<Integer> testData = Arrays.asList(-4, 6, 5, -9, 35, 0);
+
+	  int result = undertest.findClosestToZero(testData);
+	  
+	  assertThat(result).isEqualTo(0);
   }
 }
